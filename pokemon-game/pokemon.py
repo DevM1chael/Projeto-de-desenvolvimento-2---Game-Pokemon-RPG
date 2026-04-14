ed05e87b00cb09fefa65eee8af8480b2f1ea9169
Michael Henrique <michaelhenriquepedro@gmail.com>
2026-04-13 22:04:48 -0300

    Heranças de classes e subclasses

--- pokemon-game/pokemon.py
+++ pokemon-game/pokemon.py
@@ -1,2 +1,44 @@
 class Pokemon:
-    pass
+    def init(self,especie, level=1, nome=None):
+        self.especie = especie 
+        self.level = level
+
+        if nome:
+            self.nome = nome
+        else:
+            self.nome = especie
+
+    def str(self):
+        return "{} ({})".format(self.nome, self.level)
+
+    def atacar(self, pokemon):
+        print("{} atacou {}!".format(self.especie, pokemon))
+
+
+class PokemonEletrico(Pokemon):
+    tipo = "eletrico"
+
+    def atacar(self,pokemon):
+        print("{} deu um raio do trovão em {}".format(self,pokemon))
+
+class PokemonFogo(Pokemon):
+    tipo = "fogo"
+
+    def atacar(self,pokemon):
+        print("{} deu uma bola de fogo na cabeça {}".format(self,pokemon))
+
+class PokemonAgua(Pokemon):
+    tipo = "Agua"
+
+    def atacar(self,pokemon):
+        print("{} lançou um jato de agua {}".format(self,pokemon))
+
+
+meu_pokemon  =  PokemonFogo("charmander")
+meu_amigo  =  PokemonEletrico("pikachu")
+
+print(meu_pokemon, meu_pokemon.tipo)
+print(meu_amigo, meu_amigo.tipo)
+
+meu_pokemon.atacar(meu_amigo)
+meu_amigo.atacar(meu_pokemon)
