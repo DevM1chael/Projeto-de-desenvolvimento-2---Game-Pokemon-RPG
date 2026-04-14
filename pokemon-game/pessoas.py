c28793be3c923eeab81815bf73ab27be6c4efe17
Michael Henrique <michaelhenriquepedro@gmail.com>
2026-04-13 23:04:25 -0300

    Class Pessoa

---
+++ pokemon-game/pessoas.py
@@ -0,0 +1,39 @@
+from pokemon import *
+
+class Pessoa:
+    def __init__(self, nome=None, pokemons=[]):
+        if nome:
+            self.nome = nome
+
+        else:
+            self.none = "Anônimo"   
+        
+        self.pokemons = pokemons
+    
+    def __str__(self):
+        return self.nome
+    
+    def mostrar_pokemons(self):
+        if self.pokemons:
+            print("Pokemons de {}:".format(self))
+            for pokemon in self.pokemons:
+                print(pokemon)
+        else:
+            print("{} não tem nenhum pokemon".format(self))
+class Player(Pessoa):
+    tipo = "player"
+
+    def capturar(self,pokemon):
+        self.pokemons.append(pokemon)
+        print("{} capturou  {}".format(self, pokemon))
+class Inimigo(Pessoa):
+    tipo = "inimigo"
+
+
+eu = Player("felipe")
+
+pokemon_selvagem  = PokemonFogo("charmander")
+print("antes de capturar")
+eu.mostrar_pokemons()
+eu.capturar(pokemon_selvagem)
+eu.mostrar_pokemons()
