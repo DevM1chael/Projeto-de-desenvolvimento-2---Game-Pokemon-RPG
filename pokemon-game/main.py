b202a4784ffaea65740182cc9a1b759d6f7ed3e2
Michael Henrique <michaelhenriquepedro@gmail.com>
2026-04-16 00:26:43 -0300

    Mecânica de salvamento de progresso e correção de bugs.

--- pokemon-game/main.py
+++ pokemon-game/main.py
@@ -1,4 +1,5 @@
 import random
+import pickle
 from pokemon import *
 from pessoas import *
 
@@ -30,6 +31,27 @@ def escolher_pokemon_inicial(player):
         player.mostrar_pokemons()
 
 
+def salvar_jogo(player):
+    try:
+        with open("database.db", "wb") as arquivo:
+            pickle.dump(player, arquivo)
+            print("Jogo salvo com sucesso!")
+    except Exception as error:
+        print("Erro ao salvar jogo!")
+        print(error)
+
+
+def carregar_jogo():
+    try:
+        with open("database.db", "rb") as arquivo:
+            player = pickle.load(arquivo)
+            print("Jogo carregado com sucesso!")
+            return player
+    except Exception as error:
+        print("Erro ao carregar jogo!")
+        print(error)
+        return None
+
 # --- Programa Principal ---
 
 # 1. Cria o jogador
@@ -37,12 +59,17 @@ def escolher_pokemon_inicial(player):
 if __name__ == "__main__":
     print('____________________________')
     print("Bem-vindo ao mundo Pokemon!")
-    nome = input("Digite seu nome: ")
-    player = Player(nome)
     print('____________________________')
-    print("Olá {}! esse é um mundo habitado por pokemons, a partir de agora sua missão é se tornar um mestre dos pokemons".format(nome))
-    print("Capture o máximo de pokemons que conseguir e lute com seus inimigos")
-    player.mostrar_dinheiro()
+
+    player = carregar_jogo()
+
+    if not player:
+        nome = input("Digite seu nome: ")
+        player = Player(nome)
+        print('____________________________')
+        print("Olá {}! esse é um mundo habitado por pokemons, a partir de agora sua missão é se tornar um mestre dos pokemons".format(nome))
+        print("Capture o máximo de pokemons que conseguir e lute com seus inimigos")
+        player.mostrar_dinheiro()
 
     
     if player.pokemons:
@@ -55,18 +82,24 @@ if __name__ == "__main__":
     print("Pronto, agora que você já possui um pokemon, enfrente seu arqui-inimigo Gary!")
     gary = Inimigo(nome="Gary", pokemons=[PokemonAgua("Squarttle", level=1)])
     player.batalhar(gary)
+    salvar_jogo(player)
 
     while True:
         print('____________________________')
         print("O que você deseja fazer?")
         print("1 - Explorar pelo mundo a fora")
         print("2 - Batalhar com um inimigo")
+        print("3 - Ver Pokeagenda")
         print("0 - Sair do jogo")
         escolha = input("Escolha uma opção: ")
         if escolha == "1":
             player.explorar()
+            salvar_jogo(player)
         elif escolha == "2":
             player.batalhar(Inimigo())
+            salvar_jogo(player)
+        elif escolha == "3":
+            player.mostrar_pokemons()
         elif escolha == "0":
             print("Obrigado por jogar!")
             print('Fechando o jogo...')
